54297e8c6ae044700be6c39ebb4c6476dcf456d8
焦康 <Administrator@HP201-1>
2017-05-22 18:24:33 +0800

    面向对象机器人

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            robot rb1 = new robot();
+            robot rb2 = new robot();
+
+            Console.WriteLine("请选择机器人:1.小娜  2.小冰");
+            string select = Console.ReadLine();
+            switch (select)
+            {
+                
+                case "1":
+                    rb1.Name = "小娜";
+                    rb1.Eat(5);
+                    rb1.Hello();
+                    while (true)
+                    {
+                        string line = Console.ReadLine();
+                        rb1.Speak(line);
+                    }
+                    break;
+
+                case "2":
+                    rb2.Name = "小冰";
+                    rb2.Eat(5);
+                    rb2.Hello();
+                    while (true)
+                    {
+                        string line = Console.ReadLine();
+                        rb2.Speak1(line);
+                    }
+                    break;
+            }
+            
         }
     }
 }
---
+++ ConsoleApplication1/robot.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class robot
+    {
+        public string Name { get; set; }
+        private int FullLevel { get; set; }
+
+
+        public void Hello()
+        {
+            Console.WriteLine("你好,我叫{0}",Name);
+        }
+
+        public void Eat(int FoodCount)
+        {
+            if (FullLevel > 100)
+            {
+                return;
+            }
+            FullLevel = FullLevel + FoodCount;
+        }
+        public void Speak(string str)
+        {
+            if (FullLevel <=0 )
+            {
+                Console.WriteLine("我要死了");
+                return;
+            }
+            if (str.Contains("名字") || str.Contains("姓名"))
+            {
+                this.Hello();
+            }
+            else if (str.Contains("从哪里"))
+            {
+                Console.WriteLine("来自火星");
+            }
+            else if (str.Contains("女朋友"))
+            {
+                Console.WriteLine("女朋友什么的不存在的!");
+            }
+            else if (str.Contains("男的") || str.Contains("女的"))
+            {
+                Console.WriteLine("你猜,猜对了就告诉你");
+            }
+            else {
+                Console.WriteLine("你真是个哈批");
+            }
+            FullLevel--;
+        }
+
+        public void Speak1(string str)
+        {
+            if (FullLevel <= 0)
+            {
+                Console.WriteLine("我要死了");
+                return;
+            }
+            if (str.Contains("名字") || str.Contains("姓名"))
+            {
+                this.Hello();
+            }
+            else if (str.Contains("你是"))
+            {
+                Console.WriteLine("我是你朋友");
+            }
+            else if (str.Contains("男朋友"))
+            {
+                Console.WriteLine("男朋友什么的不存在的!");
+            }
+            else if (str.Contains("男的") || str.Contains("女的"))
+            {
+                Console.WriteLine("你猜,猜对了就告诉你");
+            }
+            else
+            {
+                Console.WriteLine("你真是个哈批");
+            }
+            FullLevel--;
+        }
+    }
+}
